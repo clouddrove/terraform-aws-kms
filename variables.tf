cf298cce88dadea3629101df8851b2dcf822b25d
HarmanJyot Kaur <harman@clouddrove.com>
2021-05-20 21:12:51 +0530

    removed unused variable

--- variables.tf
+++ variables.tf
@@ -50,12 +50,6 @@ variable "deletion_window_in_days" {
   description = "Duration in days after which the key is deleted after destruction of the resource."
 }
 
-variable "enable_key_rotation" {
-  type        = bool
-  default     = true
-  description = "Specifies whether key rotation is enabled."
-}
-
 variable "description" {
   type        = string
   default     = "Parameter Store KMS master key"
