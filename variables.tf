2ab3260d2078197c7e6586eeea0cb62f38349734
yadavprakash <yadavprakash.it@gmail.com>
2021-06-18 18:12:12 +0530

    update module tags

--- variables.tf
+++ variables.tf
@@ -94,3 +94,9 @@ variable "customer_master_key_spec" {
   description = "Specifies whether the key contains a symmetric key or an asymmetric key pair and the encryption algorithms or signing algorithms that the key supports. Valid values: SYMMETRIC_DEFAULT, RSA_2048, RSA_3072, RSA_4096, ECC_NIST_P256, ECC_NIST_P384, ECC_NIST_P521, or ECC_SECG_P256K1. Defaults to SYMMETRIC_DEFAULT."
   sensitive   = true
 }
+
+variable "enable_key_rotation" {
+  type        = string
+  default     = true
+  description = "Specifies whether key rotation is enabled."
+}
