9e6a893365d61465fd172e8b0188c5e51f55a316
theprashantyadav <theprashantyadav7@gmail.com>
2023-06-15 13:00:07 +0530

    feat: added description and update example.tf

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,29 @@
-## Module      : KMS KEY
-## Description : This terraform module creates a KMS Customer Master Key (CMK) and its alias.
-#output "key_arn" {
-#  value       = join("", aws_kms_key.default.*.arn)
-#  description = "Key ARN."
-#}
-#
-#output "key_id" {
-#  value       = join("", aws_kms_key.default.*.key_id)
-#  description = "Key ID."
-#}
-#
-#output "alias_arn" {
-#  value       = join("", aws_kms_alias.default.*.arn)
-#  description = "Alias ARN."
-#}
-#
-#output "alias_name" {
-#  value       = join("", aws_kms_alias.default.*.name)
-#  description = "Alias name."
-#}
-#
-#output "tags" {
-#  value       = module.labels.tags
-#  description = "A mapping of tags to assign to the resource."
-#}
-#
-#output "target_key_id" {
-#  value       = join("", aws_kms_alias.default.*.target_key_id)
-#  description = "Identifier for the key for which the alias is for, can be either an ARN or key_id."
-#}
+output "key_arn" {
+  value       = join("", aws_kms_key.default.*.arn)
+  description = "Key ARN."
+}
+
+output "key_id" {
+  value       = join("", aws_kms_key.default.*.key_id)
+  description = "Key ID."
+}
+
+output "alias_arn" {
+  value       = join("", aws_kms_alias.default.*.arn)
+  description = "Alias ARN."
+}
+
+output "alias_name" {
+  value       = join("", aws_kms_alias.default.*.name)
+  description = "Alias name."
+}
+
+output "tags" {
+  value       = module.labels.tags
+  description = "A mapping of tags to assign to the resource."
+}
+
+output "target_key_id" {
+  value       = join("", aws_kms_alias.default.*.target_key_id)
+  description = "Identifier for the key for which the alias is for, can be either an ARN or key_id."
+}
